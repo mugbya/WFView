5b291addd973eb4e7437c141cc9b960499ec5b1e
mugbya <mugbya@gmail.com>
2015-01-06 16:54:56 +0800

    init project,and replace ext-all.js

---
+++ app/core/main.js
@@ -0,0 +1,48 @@
+/**
+ * @author mugbya
+ * @version  2015-01-06
+ */
+
+/**
+ * Set the configuration for the loader.
+ * This should be called right after ext-(debug).js is included in the page, and before Ext.onReady.
+ */
+Ext.Loader.setConfig({
+    enabled : true,
+    paths : {
+        /**
+         *  设置WFUI 以及 WFTrade命名空间对应的路径
+         */
+        WFCore : 'app/core',
+        WFModule : 'app/module'
+    }
+});
+
+/**
+ * Loads Ext.app.Application class and starts it up with given configuration after the page is ready.
+ */
+Ext.application({
+    name : 'WFApp',
+    start : function(){
+        /**
+         * 启动主界面
+         */
+        Ext.create('WFCore.main.MainPanel',{
+            title : 'Activiti工作流前端界面示例'
+        });
+    },
+
+    /**
+     * Called automatically when the page has completely loaded.
+     * This is an empty function that should be overridden by each application that needs to take action on page load.
+     */
+    launch : function(){
+        /**
+         * Loads all classes by the given names and all their direct dependencies;
+         * optionally executes the given callback function when finishes, within the optional scope.
+         */
+        Ext.require('WFCore.utils.Configuration',this.start, this);
+    }
+
+});
+
